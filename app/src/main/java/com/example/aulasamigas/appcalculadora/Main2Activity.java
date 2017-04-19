192088b6ea8879e9b975df1eb55a588fead99d14
dieguk1313 <diegoferlondo1382@hotmail.com>
2017-04-18 20:42:58 -0500

    apliacion corriendo

--- app/src/main/java/com/example/aulasamigas/appcalculadora/Main2Activity.java
+++ app/src/main/java/com/example/aulasamigas/appcalculadora/Main2Activity.java
@@ -1,13 +1,62 @@
 package com.example.aulasamigas.appcalculadora;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
+
+    RadioGroup radioGroup;
+    TextView resultado;
+    EditText numberOne;
+    EditText numberTwo;
+
+    float result,valorUno,valorTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        resultado = (TextView) findViewById(R.id.textView4);
+
+        numberOne = (EditText) findViewById(R.id.editText2);
+        numberTwo = (EditText) findViewById(R.id.editText);
+        radioGroup = (RadioGroup)findViewById(R.id.grupo);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                if(checkedId == R.id.radioButton4){
+                    valorUno = Integer.parseInt(numberOne.getText().toString());
+                    valorTwo = Integer.parseInt(numberTwo.getText().toString());
+                    result = valorUno+valorTwo;
+
+                    resultado.setText(String.valueOf(result));
+                }else if(checkedId == R.id.radioButton2){
+                    valorUno = Integer.parseInt(numberOne.getText().toString());
+                    valorTwo = Integer.parseInt(numberTwo.getText().toString());
+                    result = valorUno-valorTwo;
+
+                    resultado.setText(String.valueOf(result));
+                }else if(checkedId == R.id.radioButton3){
+                    valorUno = Integer.parseInt(numberOne.getText().toString());
+                    valorTwo = Integer.parseInt(numberTwo.getText().toString());
+                    result = valorUno*valorTwo;
+
+                    resultado.setText(String.valueOf(result));
+                }else if (checkedId == R.id.radioButton) {
+                    valorUno = Integer.parseInt(numberOne.getText().toString());
+                    valorTwo = Integer.parseInt(numberTwo.getText().toString());
+                    result = valorUno / valorTwo;
+
+                    resultado.setText(String.valueOf(result));
+                }
+            }
+        });
+
     }
 }
